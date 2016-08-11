d07da7d27648303ace54a09e2afdab121f3039c1
dareno <dcreno@gmail.com>
2016-08-11 14:30:41 +0000

    last webui before node.js

--- hsec-webui/webui.py
+++ hsec-webui/webui.py
@@ -4,11 +4,58 @@
 
 # http://electronicsbyexamples.blogspot.com/2014/02/raspberry-pi-control-from-mobile-device.html
 
-from flask import Flask, render_template, request, jsonify
+
+
+
+async_mode = None
+
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
+
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+# monkey patching is necessary because this application uses a background
+# thread
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+    print("hit patching code for eventlet")
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+    print("hit patching code for gevent")
+
+
+
+import time
+from threading import Thread
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+
+
+
+
+from flask import Flask, render_template, session, request, jsonify
 import comms.comms as comms # encapsulates communication technology
-from time import sleep
 import ssl # for hand-crafted ssl context enabling TLS?
 from flask_httpauth import HTTPBasicAuth
+import json # transferring data over comms, format of data model
+#from flask.ext.socketio import SocketIO, emit
 
 users = {
     "david":"secret",
@@ -17,6 +64,89 @@ users = {
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        time.sleep(10)
+        count += 1
+        socketio.emit('my response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+
+@socketio.on('leave', namespace='/test')
+def leave(message):
+    leave_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+@socketio.on('close room', namespace='/test')
+def close(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response', {'data': 'Room ' + message['room'] + ' is closing.',
+                         'count': session['receive_count']},
+         room=message['room'])
+    close_room(message['room'])
+
+
+@socketio.on('my room event', namespace='/test')
+def send_room_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']},
+         room=message['room'])
+
+
+@socketio.on('disconnect request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected', 'count': 0})
+
+
+@sio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+
+
+
 
 @auth.get_password
 def get_pw(username):
@@ -24,14 +154,6 @@ def get_pw(username):
         return users.get(username)
     return None
 
-
-
-# return index page when IP address of RPi is typed in the browser
-@app.route("/")
-@auth.login_required
-def Index():
-    zones={"Upper Windows":"disarmed","Lower Windows":"armed","Doors":"disarmed","Inside Motion":"unarmed"}
-    return render_template("index.html", uptime=GetUptime(), zones=zones )
 
 # ajax GET call this function to set led state
 # depeding on the GET parameter sent
@@ -48,6 +170,8 @@ def arm():
         print("SendCommand(%s, disarm)" % zone)
         SendCommand([zone, "disarm"])
         pass
+    #print("global variable is hopefully one: %s" % getGlobval())
+    GetStatus()
     return ""
 
 # ajax GET call this function periodically to read button state
@@ -70,22 +194,52 @@ def GetUptime():
 
 def SendCommand(command):
     comm_channel = comms.PubChannel("tcp://*:5563")
-    sleep(1) # zmq slow joiner syndrome, should sync instead
+    time.sleep(1) # zmq slow joiner syndrome, should sync instead
     comm_channel.send("control_events", [command])
     comm_channel.close()
 
+def getComms():
+    """Opens a new listening connection if there is none yet for the
+    current application context.
+    """
+    return comms.SubChannel("tcp://state1:5564", ['sensor_events','control_events', 'state'])
+
+
 def GetStatus():
-    # create object for communication to sensor system
-    # state_comms = comms.SubChannel("tcp://state1:5564", ['sensor_events','control_events', 'state'])
-    # state_comms.close()
+    #if rv is not None:
+        #[address, contents] = rv
+        #mydata = json.loads(contents.decode('utf8'))
+        #for zone in mydata['zones']:
+            #print( "zone %s armed status: %s" % (zone, mydata['zones'][zone]['armed'] ))
     pass
-    
+
+
+
+ # return index page when IP address of RPi is typed in the browser
+@app.route("/")
+@auth.login_required
+def Index():
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.daemon = True
+        thread.start()
+
+    zones={"Upper Windows":"disarmed","Lower Windows":"armed","Doors":"disarmed","Inside Motion":"unarmed"}
+    return render_template("index.html", uptime=GetUptime(), zones=zones )
+
+@app.teardown_appcontext
+def teardown_connecteion(exception):
+    print("teardown") 
+
 # run the webserver on standard port 80, requires sudo
 if __name__ == "__main__":
     
-    #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    #context.load_cert_chain( 'ssl.cert', 'ssl.key')
-    context = ( 'hsec.crt', 'hsec.key')
-    #app.run(host='0.0.0.0', ssl_context=context, threaded=True, debug=True)
-    app.run(host='0.0.0.0', ssl_context=context, debug=True)
-    #app.run(host='0.0.0.0',debug=True)
+    #context = ( 'hsec.crt', 'hsec.key')
+    #app.run(host='0.0.0.0', ssl_context=context, debug=True, gevent=100)
+    #context = ssl.SSLContext(ssl.PROTOCOL_SSLv3 )
+    #context = ssl.SSLContext(ssl.PROTOCOL_SSLv2 )
+    #context.set_ciphers('SSL_RSA_WITH_3DES_EDE_CBC_SHA')
+    #context.load_cert_chain(certfile="hsec.crt", keyfile="hsec.key")
+    #socketio.run(app, host='0.0.0.0', ssl_context=context, debug=False)
+    socketio.run(app, host='0.0.0.0', debug=True, certfile="hsec.crt", keyfile="hsec.key")
